f38eeb79cf043396616b9d5030c8a0bf2bc1e6d8
Alexandre BARAT <alex.barat@gmail.com>
2019-11-13 17:27:03 +0100

    rails simple airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+flats_attributes = [
+  {
+    name: 'Light & Spacious Garden Flat London',
+    address: '10 Clifton Gardens London W9 1DT',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 75,
+    number_of_guests: 3
+  },
+  {
+    name: 'Stylish House Close to River Thames',
+    address: '5 Queensmill Road London SW6 6JP',
+    description: 'A beautiful place near the River Thames',
+    price_per_night: 65,
+    number_of_guests: 2
+  },
+  {
+    name: 'St Pancras Clock Tower Guest Suite',
+    address: 'Euston Rd London N1C 4QP',
+    description: 'A mini apartment within the clock tower apartment at St Pancras Station, with its own en suite bathroom and kitchen and sitting area.',
+    price_per_night: 110,
+    number_of_guests: 2
+  },
+  {
+    name: '321',
+    address: 'Top of the world',
+    description: 'My place',
+    price_per_night: 300,
+    number_of_guests: 3
+  }
+]
+Flat.create!(flats_attributes)
+puts 'Finished!'
